8c2b5c488d6426392a7b0c6401aefbb5569c07eb
peyman <pmomeni@uwaterloo.ca>
2022-02-18 18:27:15 -0500

    Added CLI commands for Submit encrypted txs

--- x/fairblock/client/cli/tx_submit_encrypted.go
+++ x/fairblock/client/cli/tx_submit_encrypted.go
@@ -1,48 +1,43 @@
 package cli
 
 import (
-	"strconv"
+	"crypto/sha256"
+	"encoding/hex"
+
+	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/tx"
 	"github.com/pememoni/FairBlock/x/fairblock/types"
-	"github.com/spf13/cobra"
 )
-
-var _ = strconv.Itoa(0)
 
 func CmdSubmitEncrypted() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "submit-encrypted [message-hash] [encryption] [target-height] [deposit]",
+		Use:   "submit-encrypted [plaintextHash] [encryption] [targetHeight] [deposit]",
 		Short: "Broadcast message submit-encrypted",
 		Args:  cobra.ExactArgs(4),
-		RunE: func(cmd *cobra.Command, args []string) (err error) {
-			argMessageHash := args[0]
-			argEncryption := args[1]
-			argTargetHeight := args[2]
-			argDeposit := args[3]
-
+		RunE: func(cmd *cobra.Command, args []string) error {
 			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
 				return err
 			}
-
-			msg := types.NewMsgSubmitEncrypted(
-				clientCtx.GetFromAddress().String(),
-				argMessageHash,
-				argEncryption,
-				argTargetHeight,
-				argDeposit,
-			)
+			// find a hash of the plaintext
+			plaintextHash := sha256.Sum256([]byte(args[0]))
+			// convert the hash to string
+			plaintextHashString := hex.EncodeToString(plaintextHash[:])
+			argsEncryption := string(args[1])
+			argsTargetHeight := string(args[2])
+			argsDeposit := string(args[3])
+			// create a new message
+			msg := types.NewMsgSubmitEncrypted(clientCtx.GetFromAddress().String(), string(plaintextHashString), string(argsEncryption), string(argsTargetHeight), string(argsDeposit))
 			if err := msg.ValidateBasic(); err != nil {
 				return err
 			}
+			// broadcast the transaction with the message to the blockchain
 			return tx.GenerateOrBroadcastTxCLI(clientCtx, cmd.Flags(), msg)
 		},
 	}
-
 	flags.AddTxFlagsToCmd(cmd)
-
 	return cmd
 }
